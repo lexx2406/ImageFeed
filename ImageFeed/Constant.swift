a88e818f88e835effddc37fb475e08f020408887
lexx2406 <lexx2406@yandex.ru>
2023-08-06 02:30:36 +0400

    Добавил файл с константами

---
+++ ImageFeed/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  ImageFeed
+//
+//  Created by Алексей Налимов on 06.08.2023.
+//
+
+import Foundation
+
+let acessKey = "<4M1hbKFrgOGW5zEGRqQEHi9sm6qGcn6o6Q55SsqHf0g>"
+let secretKey = "<PTO2GO888BScMqtZleOGlLR2ajvsLoM_bolCVnPESAU>"
+let redirectURI = "<urn:ietf:wg:oauth:2.0:oob>"
+let acessScope = "public+read_user+write_likes"
+let defaultBaseURL = URL(string: "https://api.unsplash.com")!
