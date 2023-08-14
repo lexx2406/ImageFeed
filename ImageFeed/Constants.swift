3badf6af49e7cc221b5f256943312cd461f4ccd3
lexx2406 <lexx2406@yandex.ru>
2023-08-14 05:23:26 +0400

    Подправлен файл  Constants

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-let accessKey = "<4M1hbKFrgOGW5zEGRqQEHi9sm6qGcn6o6Q55SsqHf0g>"
-let secretKey = "<PTO2GO888BScMqtZleOGlLR2ajvsLoM_bolCVnPESAU>"
-let redirectURI = "<urn:ietf:wg:oauth:2.0:oob>"
+let accessKey = "4M1hbKFrgOGW5zEGRqQEHi9sm6qGcn6o6Q55SsqHf0g"
+let secretKey = "PTO2GO888BScMqtZleOGlLR2ajvsLoM_bolCVnPESAU"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
 let accessScope = "public+read_user+write_likes"
 let defaultBaseURL = URL(string: "https://api.unsplash.com")!
