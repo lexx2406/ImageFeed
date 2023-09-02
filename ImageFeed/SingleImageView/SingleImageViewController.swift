a6e7274ad6b5b1ebd9e9c5840f884b920f693b4e
lexx2406 <lexx2406@yandex.ru>
2023-09-02 16:17:44 +0400

    Переделал констрейнты в SuperImageViewController

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -16,16 +16,8 @@ final class SingleImageViewController: UIViewController {
         }
     }
     
-    @IBOutlet private var imageView: UIImageView!
-    @IBAction private func didTapBackButton() {
-        dismiss(animated: true, completion: nil)
-    }
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBAction func didTapShareButton(_ sender: UIButton) {
-        let activityItem = [imageView.image]
-        let avc = UIActivityViewController(activityItems: activityItem as [AnyObject], applicationActivities: nil)
-        present(avc, animated: true)
-    }
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var scrollView: UIScrollView!
     
     private let alert = AlertPresenter()
     
@@ -50,45 +42,57 @@ final class SingleImageViewController: UIViewController {
             case .success(let imageResult):
                 self.rescaleAndCenterImageInScrollView(image: imageResult.image)
             case .failure:
-                self.alert.showAlert(in: self, with: AlertModel(
-                    title: "Что-то пошло не так",
-                    message: "Попробовать ещё раз?",
-                    buttonText: "Повторить",
+                    self.alert.showAlert(in: self, with: AlertModel(
+                    title: "Что-то пошло не так(",
+                    message: "Не удалось войти в систему",
+                    buttonText: "OK",
                     completion:  { action in
                         self.setImage()}
                 ),
-                                     erorr: nil)
+                                erorr: nil)
             }
             UIBlockingProgressHUD.dismiss()
         }
     }
-}
-    extension SingleImageViewController: UIScrollViewDelegate {
-            func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-                imageView
-            }
-        
-        func scrollViewDidZoom(_ scrollView: UIScrollView) {
-            let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5, 0)
-            let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5, 0)
-            scrollView.contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: 0, right: 0)
-        }
 
-        private func rescaleAndCenterImageInScrollView(image: UIImage) {
-            let minZoomScale = scrollView.minimumZoomScale
-            let maxZoomScale = scrollView.maximumZoomScale
-            view.layoutIfNeeded()
-            let visibleRectSize = scrollView.bounds.size
-            let imageSize = image.size
-            let hScale = visibleRectSize.width / imageSize.width
-            let vScale = visibleRectSize.height / imageSize.height
-            let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
-            scrollView.setZoomScale(scale, animated: false)
-            scrollView.layoutIfNeeded()
-            let newContentSize = scrollView.contentSize
-            let x = (newContentSize.width - visibleRectSize.width) / 2
-            let y = (newContentSize.height - visibleRectSize.height) / 2
-            scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
-        }
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
     }
-        
+    
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+        let activityItem = [imageView.image]
+        let avc = UIActivityViewController(activityItems: activityItem as [AnyObject], applicationActivities: nil)
+        present(avc, animated: true)
+    }
+    
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+        func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+            imageView
+        }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5, 0)
+        let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5, 0)
+        scrollView.contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: 0, right: 0)
+    }
+
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+}
+    
